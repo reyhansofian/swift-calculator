58cd5e9d83949dee244af90d28deddce8d0baf20
Reyhan Sofian <computecoholic@gmail.com>
2017-06-02 06:14:57 +0700

    Swift calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var isTyping = false
+    
+    @IBOutlet weak var display: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if isTyping {
+            let textInDisplay = display.text!
+            display.text = textInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        
+        isTyping = true
     }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        isTyping = false
+        if let mathSymbol = sender.currentTitle {
+            if mathSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
